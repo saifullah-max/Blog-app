9c40aefa525df89024a158ffdb63e060d23b9b65
Saifullah-max <saifullahahmed380@gmail.com>
2024-10-09 15:15:16 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const Blog = require("./models/blog");
+
+const app = express();
+const PORT = 8002;
+
+mongoose
+  .connect("mongodb://localhost:27017/blogify")
+  .then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  return res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, console.log(`Server started at PORT:${PORT}`));
